df2df44e85142b5b88b4b3e8318b58468c58994e
Yash404007 <yashsurve-inft@atharvacoe.ac.in>
2024-01-11 23:19:58 +0530

    database connection

---
+++ src/firebaseConfig.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDgr4sY-ucmJqJC3HWYjhx4UT6cEenRICg",
+  authDomain: "financebro-34794.firebaseapp.com",
+  databaseURL:"https://financebro-34794-default-rtdb.firebaseio.com",
+  projectId: "financebro-34794",
+  storageBucket: "financebro-34794.appspot.com",
+  messagingSenderId: "82767821313",
+  appId: "1:82767821313:web:73d4cad9f098f682b19e00",
+  measurementId: "G-6HEKFELPX8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
