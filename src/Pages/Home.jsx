d8c3256ce0267bbe3b376fea1b446e171d3caffd
Anand3074 <subramanianand-cmpn@atharvacoe.ac.in>
2024-01-14 01:46:55 +0530

    home background

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
-import {styled, Box} from '@mui/material';
 import Navbar from '../components/Layout/Navbar';
 import './home.css'
 
-const Component = styled(Box)`
-
-
-`
 
 
 
 const Home = () => {
   return (
-  <Component>
-  <Navbar/>
+    <div className="gradient">
+        <Navbar/>
+      <h1>Hello, React!</h1>
+    </div>
 
-
-  </Component>
   )
 }
 
